26f469d47178a2c3bdd1fda491163e0d4c553588
Dimas Saputra <dimas.maulanads@gmail.com>
2022-03-19 01:07:19 +0700

    run the event subscriber

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { createServer } from '@Infrastructures/http';
+import { applicationEventSubscriber } from '@Interfaces/event';
 
 (async () => {
   const server = await createServer();
   await server.start();
+
+  applicationEventSubscriber();
   console.log(`Server running at: ${server.info.uri}`);
 })();
